3cb43baa4a658e31ec49d95e72ba1f8611f87715
DaMingMing <1044096131@qq.com>
2019-03-18 00:45:05 +0800

    提交前端页面

---
+++ src/main/java/com/keymao/portal/controller/IndexController.java
@@ -0,0 +1,32 @@
+package com.keymao.portal.controller;
+
+import com.keymao.content.service.ContentService;
+import com.keymao.pojo.TbContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+//首页展示
+@Controller
+public class IndexController {
+    @Value("${CONTENT_LUNBO_ID}")
+    private Long CONTENT_LUNBO_ID;
+
+    //@Autowired
+    //private ContentService contentService;
+
+    @RequestMapping("/index")
+    public String showIndex(Model model) {
+        //查询内容列表
+/*        List<TbContent> ad1List = contentService.getContentListByCid(CONTENT_LUNBO_ID);
+        System.out.println("内容列表个数：" + ad1List.size() + ad1List.get(1).getPic());
+        model.addAttribute("ad1List", ad1List);*/
+        //把结果传递给页面
+        return "index";
+    }
+}
