44152352ac3e04699738f1fd50b695bdeb6fe60b
DaMingMing <1044096131@qq.com>
2019-06-25 01:31:24 +0800

    jsonp跨域处理

--- src/main/webapp/js/e3mall.js
+++ src/main/webapp/js/e3mall.js
@@ -1,6 +1,6 @@
 var E3MALL = {
 	checkLogin : function(){
-		var _ticket = $.cookie("TT_TOKEN");
+		var _ticket = $.cookie("token");
 		if(!_ticket){
 			return ;
 		}
@@ -11,7 +11,7 @@ var E3MALL = {
 			success : function(data){
 				if(data.status == 200){
 					var username = data.data.username;
-					var html = username + "，欢迎来到keymao购物网！<a href=\"http://www.e3mall.cn/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var html = username + "，欢迎来到keymao购物网！<a href=\"localhost:8088/user/logout.html?token=_ticket\" + '_ticket'  class=\"link-logout\">[退出]</a>";
 					$("#loginbar").html(html);
 				}
 			}
@@ -20,6 +20,8 @@ var E3MALL = {
 }
 
 $(function(){
+	//alert(123);
+	debugger;
 	// 查看是否已经登录，如果已经登录查询登录信息
 	E3MALL.checkLogin();
 });
